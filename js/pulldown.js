ba4e7b7488a768f2c0c327fc895802d49d3745e3
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-11-24 17:03:07 -0500

    touchmove added to bottom

--- js/pulldown.js
+++ js/pulldown.js
@@ -1,32 +1,47 @@
 $(function(){
+  var counter=0;
+  
   var revealHeight=60;
   $log=$('#log');
   $touchstart=$('#touchstart');
   $touchmove=$('#touchmove');
   $touchend=$('#touchend');
   
-  $('.topbar').bind('touchmove', function(e){ e.preventDefault(); });
+  $('body').bind('touchmove', function(e){ e.preventDefault(); });
+
   $('.topbar').click(function(){
-    console.log('click');
-    var off = $('.topbar').offset();
-    $log.append('<div>click '+off.top+'</div>');
-    $('.topbar').offset({top:(off.top===0)?revealHeight:0});
+    console.log('topbar');
+    var top = parseInt($('.topbar').css('top'));
+    $log.append('<div>top '+top+'</div>');
+    $('.topbar').css('top',(top===0)?revealHeight:0);
   })
+  
   $('.topbar').bind('touchstart', function(e){ 
-    $touchstart.html('touchstart '+new Date());
+    $touchstart.html('t::touchstart '+counter++);
   });
   $('.topbar').bind('touchmove', function(e){ 
-    $touchmove.html('touchmove '+new Date());
+    $touchmove.html('t::touchmove '+counter++);
   });
   $('.topbar').bind('touchend', function(e){ 
-    $touchend.html('touchend '+new Date());
+    $touchend.html('t::touchend '+counter++);
   });
-  if (0) $('.dimple').bind('touchstart touchmove touchend', function(e){ 
-    var touch = e.touches[0];
-    var node = touch.target;
-    return;
-    node.style.position = "absolute";
-    node.style.left = touch.pageX + "px";
-    node.style.top = touch.pageY + "px";
-  });  
+
+  $('.bottombar').bind('touchstart', function(e){ 
+    $touchstart.html('b::touchstart '+counter++);
+  });
+  $('.bottombar').bind('touchmove', function(e){ 
+    $touchmove.html('b::touchmove '+counter++);
+  });
+  $('.bottombar').bind('touchend', function(e){ 
+    $touchend.html('b::touchend '+counter++);
+  });
+
+
+  $('.bottombar').click(function(){
+    console.log('bottom');
+    var bot = parseInt($('.bottombar').css('bottom'));
+    $log.append('<div>bottom '+bot+'</div>');
+    $('.bottombar').css('bottom',(bot===0)?revealHeight:0);
+  })
+
 })
